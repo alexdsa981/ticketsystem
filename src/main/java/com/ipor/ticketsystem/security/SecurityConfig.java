aea995c3e4892a8f9f3e7e66a5fab52b62eabc03
Alex Saldaña <alexdsa981@gmail.com>
2024-09-08 23:22:20 -0500

    Configura roles de ejemplo con accesos GET/ DELETE

--- src/main/java/com/ipor/ticketsystem/security/SecurityConfig.java
+++ src/main/java/com/ipor/ticketsystem/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.ipor.ticketsystem.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -56,6 +57,8 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/login/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/**").hasAnyAuthority("Admin", "Usuario", "Soporte")
+                        .requestMatchers(HttpMethod.DELETE, "/api/**").hasAuthority("Admin")
                         .anyRequest().authenticated())
                 .httpBasic(withDefaults());
 
