27738e752094e771ef509343e323b4da389e49d5
Alex Saldaña <alexdsa981@gmail.com>
2024-10-24 12:41:41 -0500

    Avance actualizacion de tabla

--- src/main/java/com/ipor/ticketsystem/controller/TicketController.java
+++ src/main/java/com/ipor/ticketsystem/controller/TicketController.java
@@ -24,6 +24,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Controller
@@ -55,6 +56,7 @@ public class TicketController {
 
     public Model retornaTicketRecibidosAVista(Model model) {
         List<TicketDTO> AllTicketsDTO = ticketService.getAllTicketsSinRecepcionar();
+        Collections.reverse(AllTicketsDTO);
         model.addAttribute("AllTickets", AllTicketsDTO);
         return model;
     }
--- src/main/resources/static/js/wsActualizaTabla.js
+++ src/main/resources/static/js/wsActualizaTabla.js
@@ -1,28 +1,24 @@
 
-   // Contador basado en la cantidad de tickets cargados inicialmente
-    let ticketCounter = document.querySelectorAll('#ticketTableBody tr').length + 1;
-
-    // Función para manejar el ticket recibido
+// Función para manejar el ticket recibido
 export function ActualizaTablaRecibidos(ticketRecord) {
-        console.log('Ticket recibido:', ticketRecord);
+    console.log('Ticket recibido:', ticketRecord);
 
-        // Obtener el cuerpo de la tabla donde se agregarán las filas
-        const ticketTableBody = document.getElementById('ticketTableBody');
+    // Obtener el cuerpo de la tabla donde se agregarán las filas
+    const ticketTableBody = document.getElementById('ticketTableBody');
 
-        // Crear una nueva fila
-        const newRow = document.createElement('tr');
+    // Crear una nueva fila
+    const newRow = document.createElement('tr');
 
-        // Generar las celdas de la fila con los datos del ticket
-        newRow.innerHTML = `
-            <td>${ticketCounter++}</td>
-            <td>${ticketRecord.fechaFormateada}</td>
-            <td>${ticketRecord.horaFormateada}</td>
-            <td>${ticketRecord.nombreUsuario}</td>
+    // Generar las celdas de la fila con los datos del ticket
+    newRow.innerHTML = `
+        <td></td> <!-- Este número se actualizará luego -->
+        <td>${ticketRecord.fechaFormateada}</td>
+        <td>${ticketRecord.horaFormateada}</td>
+        <td>${ticketRecord.nombreUsuario}</td>
         <td>
             <span class="nombreClasificacionIncidencia">${ticketRecord.nombreClasificacionIncidencia}:<br></span>
             <span>${ticketRecord.descripcion}</span><br>
         </td>
-
         <td>
             <ul>
                 ${ticketRecord.listaArchivosAdjuntos.map(adjunto => `
@@ -46,8 +42,34 @@ export function ActualizaTablaRecibidos(ticketRecord) {
                 Recepción
             </button>
         </td>
-        `;
+    `;
 
-        // Agregar la nueva fila al cuerpo de la tabla
-        ticketTableBody.appendChild(newRow);
-    }
+    // Insertar la nueva fila al inicio de la tabla
+    ticketTableBody.insertBefore(newRow, ticketTableBody.firstChild);
+
+    // Actualizar el número de cada fila
+    const rows = ticketTableBody.querySelectorAll('tr');
+    rows.forEach((row, index) => {
+        row.querySelector('td').textContent = index + 1;
+    });
+
+    // Añadir clase de fondo verde suave a cada celda (td) de la nueva fila
+    const cells = newRow.querySelectorAll('td');
+    cells.forEach(cell => {
+        cell.classList.add('nuevo-ticket');
+    });
+
+    // Añadir evento para cambiar el fondo de cada celda a blanco de manera irreversible
+    newRow.addEventListener('mouseover', function () {
+        cells.forEach(cell => {
+            cell.classList.remove('nuevo-ticket');
+            cell.classList.add('fondo-blanco'); // Cambiar el fondo de cada celda a blanco
+        });
+    });
+
+    // Actualiza la lista de filas filtradas
+    filteredRows.unshift(newRow); // Añadir la nueva fila al principio del arreglo de filas filtradas
+
+    // Llama a la función para actualizar las filas y la paginación
+    updateFilteredRows();
+}
