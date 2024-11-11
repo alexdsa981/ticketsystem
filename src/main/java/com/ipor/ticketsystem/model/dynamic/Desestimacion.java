7ddc9d3a1e9c66ec2d377afb35aa8d418751b7cd
Alex Saldaña <alexdsa981@gmail.com>
2024-11-10 21:53:42 -0500

    agrega entidades desestimacion y clasificacion_desestimacion

---
+++ src/main/java/com/ipor/ticketsystem/model/dynamic/Desestimacion.java
@@ -0,0 +1,45 @@
+package com.ipor.ticketsystem.model.dynamic;
+import com.ipor.ticketsystem.model.fixed.ClasificacionDesestimacion;
+import com.ipor.ticketsystem.model.fixed.ClasificacionServicio;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Desestimacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private LocalDate fecha;
+    @Column(nullable = false)
+    private LocalTime hora;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(MAX)")
+    private String descripcion;
+
+    @ManyToOne
+    @JoinColumn(name= "id_usuario", nullable = false)
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name= "id_clasificacion_desestimacion", nullable = false)
+    private ClasificacionDesestimacion clasificacionDesestimacion;
+
+    @OneToOne
+    @JoinColumn(name = "id_ticket", nullable = false)
+    private Ticket ticket;
+
+    @PrePersist
+    public void prePersist() {
+        this.fecha = LocalDate.now();
+        this.hora = LocalTime.now();
+    }
+}
