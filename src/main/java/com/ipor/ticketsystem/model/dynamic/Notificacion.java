ffa05be8773b97021556f447899ed4522fa7a3ce
Alex Saldaña <alexdsa981@gmail.com>
2024-12-19 13:59:43 -0500

    agrega clase/tabla notificacion

---
+++ src/main/java/com/ipor/ticketsystem/model/dynamic/Notificacion.java
@@ -0,0 +1,49 @@
+package com.ipor.ticketsystem.model.dynamic;
+
+import com.ipor.ticketsystem.model.fixed.ClasificacionIncidencia;
+import com.ipor.ticketsystem.model.fixed.FaseTicket;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Notificacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private LocalDate fecha;
+    @Column(nullable = false)
+    private LocalTime hora;
+    @Column(nullable = true)
+    private Boolean leido;
+    @Column(nullable = true)
+    private Boolean abierto;
+    @Column(nullable = true)
+    private String url;
+    @Column(nullable = false, columnDefinition = "VARCHAR(MAX)")
+    private String mensaje;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private Usuario usuario;
+    @OneToOne
+    @JoinColumn(name = "id_ticket", nullable = false)
+    private Ticket ticket;
+
+
+    // Método para establecer fecha y hora actuales antes de persistir
+    @PrePersist
+    public void prePersist() {
+        this.fecha = LocalDate.now();
+        this.hora = LocalTime.now();
+    }
+}
