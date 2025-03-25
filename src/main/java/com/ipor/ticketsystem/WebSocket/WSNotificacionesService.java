c1f7064eea9255b308c53b15b20f0cf7ad6e41dd
Alex Saldaña <alexdsa981@gmail.com>
2025-03-24 22:46:40 -0500

    240325

--- src/main/java/com/ipor/ticketsystem/WebSocket/WSNotificacionesService.java
+++ src/main/java/com/ipor/ticketsystem/WebSocket/WSNotificacionesService.java
@@ -12,8 +12,7 @@ import com.ipor.ticketsystem.model.dynamic.Notificacion;
 import com.ipor.ticketsystem.model.dynamic.Recepcion;
 import com.ipor.ticketsystem.model.dynamic.Servicio;
 import com.ipor.ticketsystem.service.TicketService;
-import com.ipor.ticketsystem.service.UsuarioService;
-import jdk.swing.interop.SwingInterOpUtils;
+
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
